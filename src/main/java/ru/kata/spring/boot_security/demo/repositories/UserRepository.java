e8f08e56b05b4af57f99fc27dab225fc4dd30797
Ambjorn97 <turpuhanov.amir@yandex.ru>
2024-11-18 18:02:19 +0300

    fix

--- src/main/java/ru/kata/spring/boot_security/demo/repositories/UserRepository.java
+++ src/main/java/ru/kata/spring/boot_security/demo/repositories/UserRepository.java
@@ -2,6 +2,7 @@ package ru.kata.spring.boot_security.demo.repositories;
 
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import ru.kata.spring.boot_security.demo.models.User;
 
@@ -17,6 +18,7 @@ public interface UserRepository extends JpaRepository<User, Integer> {
 
     void deleteById(int id);
 
+    @Query("SELECT u from User u join fetch u.roles")
     List<User> findAll();
 
 }
